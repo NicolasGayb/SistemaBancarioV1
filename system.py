6945cfee2780a8abc0f7545252eb52bd88134204
NicolasGayb <nicolasgbleal@gmail.com>
2024-05-03 20:12:51 -0300

    Operação de depósito adicionada

--- system.py
+++ system.py
@@ -12,13 +12,19 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+deposito = float
 
 while True:
 
     opcao = input(menu)
 
     if opcao == "d":
-        print("Depósito")
+        valor = float(input("Insira o valor que deseja depositar: "))
+        if valor > 0:
+            print(f"O valor de {valor} foi depositado em sua conta!")
+        
+        else:
+            print("O valor informado para depósito é inválido, favor informar um valor maior que 0 para prosseguir com a operação.")
 
     elif opcao == "s":
         print("Saque")
